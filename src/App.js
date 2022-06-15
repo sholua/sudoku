9ac9f0afedd044ff4c57ee90c07b99208709b7f3
Oleksandr <s471652@gmail.com>
2022-06-15 22:37:20 +0300

    Media Query Manager

--- src/App.js
+++ src/App.js
@@ -78,6 +78,43 @@ function App() {
           <input className="cell cell__mute" value={8} />
           <input className="cell cell__mute" value={9} />
         </div>
+        <div className="sudoku-row">
+          <input className="cell cell__mute" type="text" value={1} readOnly />
+          <input className="cell" type="text" value={""} />
+          <input
+            className="cell cell__horizontal-verge"
+            type="text"
+            value={3}
+          />
+          <input className="cell cell__mute" value={4} />
+          <input className="cell" value={5} />
+          <input className="cell cell__horizontal-verge" value={6} />
+          <input className="cell cell__mute" value={7} />
+          <input className="cell" value={8} />
+          <input className="cell cell__mute" value={9} />
+        </div>
+        <div className="sudoku-row">
+          <input className="cell" value={1} />
+          <input className="cell cell__mute" value={2} />
+          <input className="cell cell__horizontal-verge" value={3} />
+          <input className="cell" value={4} />
+          <input className="cell" value={5} />
+          <input className="cell cell__horizontal-verge" value={6} />
+          <input className="cell cell__mute" value={7} />
+          <input className="cell" value={8} />
+          <input className="cell cell__mute" value={9} />
+        </div>
+        <div className="sudoku-row">
+          <input className="cell" value={1} />
+          <input className="cell" value={2} />
+          <input className="cell cell__horizontal-verge" value={3} />
+          <input className="cell" value={4} />
+          <input className="cell" value={5} />
+          <input className="cell cell__horizontal-verge" value={6} />
+          <input className="cell" value={7} />
+          <input className="cell cell__mute" value={8} />
+          <input className="cell cell__mute" value={9} />
+        </div>
         <button className="btn u-margin-top-medium u-margin-bottom-medium">
           Create New Puzzle
         </button>
