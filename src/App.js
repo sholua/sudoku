2be64a18a95df39dac7f192f17d2aa0c268241ff
Oleksandr <s471652@gmail.com>
2022-06-16 16:09:34 +0300

    Logo box + responsive design

--- src/App.js
+++ src/App.js
@@ -4,116 +4,126 @@ function App() {
   return (
     <div className="game-layout u-text-center">
       <div className="sudoku">
+        <div className="logo-box">
+          <div className="logo-box__logo">
+            <img className="" src="logo.png" alt="logo" />
+          </div>
+          <img
+            className="logo-box__logo-text"
+            src="logo-text.png"
+            alt="logo-text"
+          />
+        </div>
         <div className="sudoku-row">
-          <input className="cell cell__mute" type="text" value={1} readOnly />
+          <input className="cell cell--mute" type="text" value={1} readOnly />
           <input className="cell" type="text" value={""} />
           <input
-            className="cell cell__horizontal-verge"
+            className="cell cell--horizontal-verge"
             type="text"
             value={3}
           />
-          <input className="cell cell__mute" value={4} />
+          <input className="cell cell--mute" value={4} />
           <input className="cell" value={5} />
-          <input className="cell cell__horizontal-verge" value={6} />
-          <input className="cell cell__mute" value={7} />
-          <input className="cell cell__error" value={8} />
-          <input className="cell cell__mute" value={9} />
+          <input className="cell cell--horizontal-verge" value={6} />
+          <input className="cell cell--mute" value={7} />
+          <input className="cell cell--error" value={8} />
+          <input className="cell cell--mute" value={9} />
         </div>
         <div className="sudoku-row">
           <input className="cell" value={1} />
-          <input className="cell cell__mute" value={2} />
-          <input className="cell cell__horizontal-verge" value={3} />
+          <input className="cell cell--mute" value={2} />
+          <input className="cell cell--horizontal-verge" value={3} />
           <input className="cell" value={4} />
           <input className="cell" value={5} />
-          <input className="cell cell__horizontal-verge" value={6} />
-          <input className="cell cell__mute" value={7} />
+          <input className="cell cell--horizontal-verge" value={6} />
+          <input className="cell cell--mute" value={7} />
           <input className="cell" value={8} />
-          <input className="cell cell__mute" value={9} />
+          <input className="cell cell--mute" value={9} />
         </div>
         <div className="sudoku-row">
           <input className="cell" value={1} />
           <input className="cell" value={2} />
-          <input className="cell cell__horizontal-verge" value={3} />
+          <input className="cell cell--horizontal-verge" value={3} />
           <input className="cell" value={4} />
           <input className="cell" value={5} />
-          <input className="cell cell__horizontal-verge" value={6} />
+          <input className="cell cell--horizontal-verge" value={6} />
           <input className="cell" value={7} />
-          <input className="cell cell__mute" value={8} />
-          <input className="cell cell__mute" value={9} />
+          <input className="cell cell--mute" value={8} />
+          <input className="cell cell--mute" value={9} />
         </div>
         <div className="sudoku-row">
-          <input className="cell cell__mute" type="text" value={1} readOnly />
+          <input className="cell cell--mute" type="text" value={1} readOnly />
           <input className="cell" type="text" value={""} />
           <input
-            className="cell cell__horizontal-verge"
+            className="cell cell--horizontal-verge"
             type="text"
             value={3}
           />
-          <input className="cell cell__mute" value={4} />
+          <input className="cell cell--mute" value={4} />
           <input className="cell" value={5} />
-          <input className="cell cell__horizontal-verge" value={6} />
-          <input className="cell cell__mute" value={7} />
+          <input className="cell cell--horizontal-verge" value={6} />
+          <input className="cell cell--mute" value={7} />
           <input className="cell" value={8} />
-          <input className="cell cell__mute" value={9} />
+          <input className="cell cell--mute" value={9} />
         </div>
         <div className="sudoku-row">
           <input className="cell" value={1} />
-          <input className="cell cell__mute" value={2} />
-          <input className="cell cell__horizontal-verge" value={3} />
+          <input className="cell cell--mute" value={2} />
+          <input className="cell cell--horizontal-verge" value={3} />
           <input className="cell" value={4} />
           <input className="cell" value={5} />
-          <input className="cell cell__horizontal-verge" value={6} />
-          <input className="cell cell__mute" value={7} />
+          <input className="cell cell--horizontal-verge" value={6} />
+          <input className="cell cell--mute" value={7} />
           <input className="cell" value={8} />
-          <input className="cell cell__mute" value={9} />
+          <input className="cell cell--mute" value={9} />
         </div>
         <div className="sudoku-row">
           <input className="cell" value={1} />
           <input className="cell" value={2} />
-          <input className="cell cell__horizontal-verge" value={3} />
+          <input className="cell cell--horizontal-verge" value={3} />
           <input className="cell" value={4} />
           <input className="cell" value={5} />
-          <input className="cell cell__horizontal-verge" value={6} />
+          <input className="cell cell--horizontal-verge" value={6} />
           <input className="cell" value={7} />
-          <input className="cell cell__mute" value={8} />
-          <input className="cell cell__mute" value={9} />
+          <input className="cell cell--mute" value={8} />
+          <input className="cell cell--mute" value={9} />
         </div>
         <div className="sudoku-row">
-          <input className="cell cell__mute" type="text" value={1} readOnly />
+          <input className="cell cell--mute" type="text" value={1} readOnly />
           <input className="cell" type="text" value={""} />
           <input
-            className="cell cell__horizontal-verge"
+            className="cell cell--horizontal-verge"
             type="text"
             value={3}
           />
-          <input className="cell cell__mute" value={4} />
+          <input className="cell cell--mute" value={4} />
           <input className="cell" value={5} />
-          <input className="cell cell__horizontal-verge" value={6} />
-          <input className="cell cell__mute" value={7} />
+          <input className="cell cell--horizontal-verge" value={6} />
+          <input className="cell cell--mute" value={7} />
           <input className="cell" value={8} />
-          <input className="cell cell__mute" value={9} />
+          <input className="cell cell--mute" value={9} />
         </div>
         <div className="sudoku-row">
           <input className="cell" value={1} />
-          <input className="cell cell__mute" value={2} />
-          <input className="cell cell__horizontal-verge" value={3} />
+          <input className="cell cell--mute" value={2} />
+          <input className="cell cell--horizontal-verge" value={3} />
           <input className="cell" value={4} />
           <input className="cell" value={5} />
-          <input className="cell cell__horizontal-verge" value={6} />
-          <input className="cell cell__mute" value={7} />
+          <input className="cell cell--horizontal-verge" value={6} />
+          <input className="cell cell--mute" value={7} />
           <input className="cell" value={8} />
-          <input className="cell cell__mute" value={9} />
+          <input className="cell cell--mute" value={9} />
         </div>
         <div className="sudoku-row">
           <input className="cell" value={1} />
           <input className="cell" value={2} />
-          <input className="cell cell__horizontal-verge" value={3} />
+          <input className="cell cell--horizontal-verge" value={3} />
           <input className="cell" value={4} />
           <input className="cell" value={5} />
-          <input className="cell cell__horizontal-verge" value={6} />
+          <input className="cell cell--horizontal-verge" value={6} />
           <input className="cell" value={7} />
-          <input className="cell cell__mute" value={8} />
-          <input className="cell cell__mute" value={9} />
+          <input className="cell cell--mute" value={8} />
+          <input className="cell cell--mute" value={9} />
         </div>
         <button className="btn u-margin-top-medium u-margin-bottom-medium">
           Create New Puzzle
